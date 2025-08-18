2d39cea28694bb93e3310236bff22b6a4d006cf2
atsmacode <87829757+atsmacode@users.noreply.github.com>
2025-08-18 19:03:04 +0100

    Remove timestamps

--- database/migrations/2022_03_08_111411_create_streets_table.php
+++ database/migrations/2022_03_08_111411_create_streets_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('streets', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->timestamps();
         });
     }
 
