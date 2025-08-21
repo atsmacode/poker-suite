7124490b9dcbc59761296a1fcd8ba92c427e0641
Mark <mark@Marks-MacBook-Pro.local>
2025-08-21 15:13:41 +0100

    Add ace high case

--- app/Enums/Rank.php
+++ app/Enums/Rank.php
@@ -8,21 +8,20 @@ enum Rank: int
 {
     use CanRandomiseCases;
 
-    case ACE   = 1;
-    case DEUCE = 2;
-    case THREE = 3;
-    case FOUR  = 4;
-    case FIVE  = 5;
-    case SIX   = 6;
-    case SEVEN = 7;
-    case EIGHT = 8;
-    case NINE  = 9;
-    case TEN   = 10;
-    case JACK  = 11;
-    case QUEEN = 12;
-    case KING  = 13;
-
-    public const ACE_HIGH = 14;
+    case ACE      = 1;
+    case DEUCE    = 2;
+    case THREE    = 3;
+    case FOUR     = 4;
+    case FIVE     = 5;
+    case SIX      = 6;
+    case SEVEN    = 7;
+    case EIGHT    = 8;
+    case NINE     = 9;
+    case TEN      = 10;
+    case JACK     = 11;
+    case QUEEN    = 12;
+    case KING     = 13;
+    case ACE_HIGH = 14;
 
     public function toArray(): array
     {
@@ -37,38 +36,40 @@ enum Rank: int
     public function name(): string
     {
         return match ($this) {
-            self::ACE   => 'Ace',
-            self::DEUCE => 'Deuce',
-            self::THREE => 'Three',
-            self::FOUR  => 'Four',
-            self::FIVE  => 'Five',
-            self::SIX   => 'Six',
-            self::SEVEN => 'Seven',
-            self::EIGHT => 'Eight',
-            self::NINE  => 'Nine',
-            self::TEN   => 'Ten',
-            self::JACK  => 'Jack',
-            self::QUEEN => 'Queen',
-            self::KING  => 'King',
+            self::ACE      => 'Ace',
+            self::DEUCE    => 'Deuce',
+            self::THREE    => 'Three',
+            self::FOUR     => 'Four',
+            self::FIVE     => 'Five',
+            self::SIX      => 'Six',
+            self::SEVEN    => 'Seven',
+            self::EIGHT    => 'Eight',
+            self::NINE     => 'Nine',
+            self::TEN      => 'Ten',
+            self::JACK     => 'Jack',
+            self::QUEEN    => 'Queen',
+            self::KING     => 'King',
+            self::ACE_HIGH => 'Ace High',
         };
     }
 
     public function abbreviation(): string
     {
         return match ($this) {
-            self::ACE   => 'A',
-            self::DEUCE => '2',
-            self::THREE => '3',
-            self::FOUR  => '4',
-            self::FIVE  => '5',
-            self::SIX   => '6',
-            self::SEVEN => '7',
-            self::EIGHT => '8',
-            self::NINE  => '9',
-            self::TEN   => '10',
-            self::JACK  => 'J',
-            self::QUEEN => 'Q',
-            self::KING  => 'K',
+            self::ACE        => 'A',
+            self::DEUCE      => '2',
+            self::THREE      => '3',
+            self::FOUR       => '4',
+            self::FIVE       => '5',
+            self::SIX        => '6',
+            self::SEVEN      => '7',
+            self::EIGHT      => '8',
+            self::NINE       => '9',
+            self::TEN        => '10',
+            self::JACK       => 'J',
+            self::QUEEN      => 'Q',
+            self::KING       => 'K',
+            self::ACE_HIGH   => 'A',
         };
     }
 }
