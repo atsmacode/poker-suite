18212c90defffc182ce81838272bd62f7bc9749b
Mark <mark@Marks-MacBook-Pro.local>
2025-08-30 10:09:32 +0100

    Remove unused dep

--- app/Handlers/ShowGameHandler.php
+++ app/Handlers/ShowGameHandler.php
@@ -5,12 +5,10 @@ namespace App\Handlers;
 use App\Http\Resources\GameStateResource;
 use App\Models\Game;
 use App\Services\GamePlayService;
-use App\Services\GameSetupService;
 
 class ShowGameHandler
 {
     public function __construct(
-        private GameSetupService $gameSetup,
         private GamePlayService $gamePlay
     ) {
     }
