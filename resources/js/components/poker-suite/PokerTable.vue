57dea66ca738e4f3fd2d0a497fe5328fb4ab6f66
Mark <mark@Marks-MacBook-Pro.local>
2025-09-02 09:06:12 +0100

    Add colour & additional sections

--- resources/js/components/poker-suite/PokerTable.vue
+++ resources/js/components/poker-suite/PokerTable.vue
@@ -3,9 +3,19 @@ const { seatOrder } = defineProps(['seatOrder']);
 </script>
 <template>
     <div>
-        <div class="border rounded-full m-10">
-            <div v-for="(seats, index) in seatOrder" :class="{'mb-60': index == 0}" class="flex justify-between gap-4 m-10">
-                <div v-for="seat in seats" class="rounded-xl p-4 border h-20 w-30">Seat #{{ seat.number }}</div>
+        <div class="relative border-30 border-emerald-900 rounded-full m-10 bg-emerald-950 text-center">
+            <div class="flex justify-between gap-4 m-5">
+                <div v-for="seat in seatOrder[0]" class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">Seat #{{ seat.number }}</div>
+            </div>
+            <div class="grid grid-cols-12 m-2 text-left">
+                <span class="font-extrabold text-xl col-start-4 col-end-6">Pot: #</span>
+                <span class="font-extrabold text-xl col-start-8 col-end-10">Other info: ...</span>
+            </div>
+            <div class="grid grid-cols-12">
+                <span class="border border-emerald-900 rounded h-30 col-start-3 col-end-11 p-2">Community cards</span>
+            </div>
+            <div class="flex justify-between gap-4 m-5">
+                <div v-for="seat in seatOrder[1]" class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">Seat #{{ seat.number }}</div>
             </div>
         </div>
     </div>
