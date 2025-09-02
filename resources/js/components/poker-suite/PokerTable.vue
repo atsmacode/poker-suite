0745319829919d7d9eaa2e044b2d221192e9fe2b
Mark <mark@Marks-MacBook-Pro.local>
2025-09-02 09:22:30 +0100

    Add table seat component

--- resources/js/components/poker-suite/PokerTable.vue
+++ resources/js/components/poker-suite/PokerTable.vue
@@ -1,11 +1,13 @@
 <script setup>
+import TableSeat from './TableSeat.vue';
+
 const { seatOrder } = defineProps(['seatOrder']);
 </script>
 <template>
     <div>
         <div class="relative border-30 border-emerald-900 rounded-full m-10 bg-emerald-950 text-center">
             <div class="flex justify-between gap-4 m-5">
-                <div v-for="seat in seatOrder[0]" class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">Seat #{{ seat.number }}</div>
+                <TableSeat v-for="seat in seatOrder[0]" :seat />
             </div>
             <div class="grid grid-cols-12 m-2 text-left">
                 <span class="font-extrabold text-xl col-start-4 col-end-6">Pot: #</span>
@@ -15,7 +17,7 @@ const { seatOrder } = defineProps(['seatOrder']);
                 <span class="border border-emerald-900 rounded h-30 col-start-3 col-end-11 p-2">Community cards</span>
             </div>
             <div class="flex justify-between gap-4 m-5">
-                <div v-for="seat in seatOrder[1]" class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">Seat #{{ seat.number }}</div>
+                <TableSeat v-for="seat in seatOrder[1]" :seat />
             </div>
         </div>
     </div>
---
+++ resources/js/components/poker-suite/TableSeat.vue
@@ -0,0 +1,16 @@
+<script setup>
+const { seat, player } = defineProps(['seat', 'player']);
+</script>
+<template>
+    <div class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">
+        <div class="grid grid-cols-2">
+            <div class="border rounded w10 h-15 m-1">
+            </div>
+            <div class="border rounded w10 h-15 m-1">
+            </div>
+        </div>
+        <div class="p-2">
+            #{{ seat.number }} {{ seat.player_id ? seat.player_id : 'Empty' }}
+        </div>
+    </div>
+</template>
