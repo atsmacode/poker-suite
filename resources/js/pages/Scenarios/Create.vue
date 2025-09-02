391c381e80c4caf29c7a111924d8c6de0fbc744f
Mark <mark@Marks-MacBook-Pro.local>
2025-09-02 09:43:47 +0100

    Add placeholder text

--- resources/js/pages/Scenarios/Create.vue
+++ resources/js/pages/Scenarios/Create.vue
@@ -47,7 +47,20 @@ setupGame();
                 <PokerTable :seatOrder />
             </div>
             <div class="basis-1/3 p-4 border rounded-xl h-full">
-                Data
+                <div class="mb-10">
+                    <p class="border-b mb-2 text-xl">Poker Calculations</p>
+                    <ul>
+                        <li>Pot Odds</li>
+                        <li>Outs</li>
+                        <li>...</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p class="border-b mb-2 text-xl">AI Overview</p>
+                    <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.</p>
+                    <p>Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.</p>
+                </div>
             </div>
         </div>
     </AppLayout>
