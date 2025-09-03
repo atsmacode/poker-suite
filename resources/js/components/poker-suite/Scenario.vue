826655388e0d031dd65c35894b9e358f2bc64ffc
Mark <mark@Marks-MacBook-Pro.local>
2025-09-03 11:04:37 +0100

    Remove player prop, default 6

--- resources/js/components/poker-suite/Scenario.vue
+++ resources/js/components/poker-suite/Scenario.vue
@@ -4,7 +4,7 @@ import PokerTable from './PokerTable.vue';
 import SelectSeatCount from './SelectSeatCount.vue';
 import { watch, ref } from 'vue';
 
-const { seatOrder, selectedSeatCount } = defineProps({seatOrder: Object, selectedSeatCount: Number});
+const { seatOrder, selectedSeatCount } = defineProps({seatOrder: Object, selectedSeatCount: {type: Number, default: 6}});
 const emit = defineEmits(['seatsChanged']);
 const tableSeatCount = ref(0);
 
@@ -19,7 +19,7 @@ watch(tableSeatCount, (newCount) => { emit('seatsChanged', newCount) });
         </div>
         <div class="flex flex-row gap-4 h-full mx-4">
             <div class="gap-4 basis-2/3">
-                <PokerTable :seatOrder :player="null" />
+                <PokerTable :seatOrder />
             </div>
             <div class="basis-1/3 p-4 border rounded-xl h-full">
                 <GameSidePanel />
