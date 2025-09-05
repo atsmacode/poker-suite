a8f8735436564247aeb506dd0f1a7efe9f65ba10
Mark <mark@Marks-MacBook-Pro.local>
2025-09-05 10:06:11 +0100

    Style games index

--- resources/js/pages/Games/Index.vue
+++ resources/js/pages/Games/Index.vue
@@ -26,22 +26,31 @@ defineProps({games: Object});
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                 <table class="table-auto w-full">
-                <thead>
-                    <tr >
-                        <th>ID</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="border" v-for="game in games">
-                        <td>
-                            {{ game.id }}
-                        </td>
-                        <td>
-                            <TextLink :href="route('games.show', game.id )">Open</TextLink>
-                        </td>
-                    </tr>
-                </tbody>
+                    <thead>
+                        <tr class="text-left">
+                            <th>ID</th>
+                            <th>Status</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-if="games.length > 0" class="border-y bg-[#111111]" v-for="(game, n) in games" :class="{'bg-[#202020]': (n/2) % 1}">
+                            <td>
+                                {{ game.id }}
+                            </td>
+                            <td>
+                                Created
+                            </td>
+                            <td>
+                                <TextLink :href="route('games.show', game.id )">Open</TextLink>
+                            </td>
+                        </tr>
+                        <tr v-else>
+                            <td>No Games to display...</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
