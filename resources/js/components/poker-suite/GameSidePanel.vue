3406daa80c792052b25c967dd16a5226737befbe
Mark <mark@Marks-MacBook-Pro.local>
2025-09-05 08:47:32 +0100

    Edit side panel text

--- resources/js/components/poker-suite/GameSidePanel.vue
+++ resources/js/components/poker-suite/GameSidePanel.vue
@@ -4,18 +4,23 @@
 <template>
 <div>
     <div class="mb-10">
+        <p class="border-b mb-2 text-xl">Guide</p>
+        <p class="text-sm">Add players, cards and actions and find out your odds of winning the hand in that scenario, and get AI advice on what to do.</p>
+    </div>
+    <div class="mb-10">
         <p class="border-b mb-2 text-xl">Poker Calculations</p>
-        <ul>
-            <li>Pot Odds</li>
-            <li>Outs</li>
-            <li>...</li>
+        <p class="text-sm">Here's your odd of winning the hand:</p>
+        <br/>
+        <ul class="text-sm">
+            <li>Risk VS reward ...</li>
+            <li>Cards that would improve you are ...</li>
+            <li>And more ...</li>
         </ul>
     </div>
 
     <div>
-        <p class="border-b mb-2 text-xl">AI Overview</p>
-        <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.</p>
-        <p>Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas.</p>
+        <p class="border-b mb-2 text-xl">AI Advice</p>
+        <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis...</p>
     </div>
 </div>
 </template>
