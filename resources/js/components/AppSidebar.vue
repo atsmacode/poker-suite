6649f0e5f086d884a88f26177e9ab0b9b47def52
Mark <mark@Marks-MacBook-Pro.local>
2025-09-05 07:39:26 +0100

    Use spade/test icons

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -5,24 +5,19 @@ import NavUser from '@/components/NavUser.vue';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, Spade, TestTubeDiagonal } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-    {
         title: 'Games',
         href: '/games',
-        icon: LayoutGrid,
+        icon: Spade,
     },
     {
         title: 'Scenarios',
         href: '/scenarios',
-        icon: LayoutGrid,
+        icon: TestTubeDiagonal,
     },
 ];
 
@@ -46,7 +41,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="route('dashboard')">
+                        <Link :href="route('games.index')">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
