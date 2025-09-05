945ce257368b07494c99215a3abeb8f9840d0fb0
Mark <mark@Marks-MacBook-Pro.local>
2025-09-05 08:06:53 +0100

    Small text

--- resources/js/components/poker-suite/TableSeat.vue
+++ resources/js/components/poker-suite/TableSeat.vue
@@ -6,8 +6,8 @@ const { seat, player } = defineProps({seat: Object, player: {type: Object, defau
 <template>
     <div class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">
         <HoleCards />
-        <div class="p-2">
-            #{{ seat.number }} {{ seat.player_id ? player?.name : 'Empty' }}
+        <div class="p-2 text-xs">
+            {{ seat.player_id ? player?.name : 'Empty Seat' }}
         </div>
     </div>
 </template>
