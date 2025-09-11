ee7f0ed41c7810767fb1c3b14d24b5322366c621
Mark <mark@Marks-MacBook-Pro.local>
2025-09-11 10:32:44 +0100

    Remove hole cards comp, use Card

---
+++ resources/js/components/poker-suite/Card.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+    <div class="border rounded w10 h-15 m-1"></div>
+</template>
--- resources/js/components/poker-suite/TableSeat.vue
+++ resources/js/components/poker-suite/TableSeat.vue
@@ -1,11 +1,14 @@
 <script setup>
-import HoleCards from './HoleCards.vue';
+import Card from './Card.vue';
 
 const { seat, player } = defineProps({seat: Object, player: {type: Object, default: null}});
 </script>
 <template>
     <div class="rounded-xl p-2 border h-30 w-30 bg-black shadow-xl">
-        <HoleCards />
+        <div class="grid grid-cols-2">
+            <Card />
+            <Card />
+        </div>
         <div class="p-2 text-xs">
             {{ seat.player_id ? player?.name : 'Empty Seat' }}
         </div>
