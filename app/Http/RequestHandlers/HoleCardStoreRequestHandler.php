2094842568d02b09d3520298fc7ec9267dc35a6e
Mark <mark@Marks-MacBook-Pro.local>
2025-09-23 08:31:51 +0100

    Call builder

--- app/Http/RequestHandlers/HoleCardStoreRequestHandler.php
+++ app/Http/RequestHandlers/HoleCardStoreRequestHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\RequestHandlers;
 
+use App\Builders\HoleCardBuilder;
 use App\GamePlay\GameState;
 use App\Http\Requests\HoleCardStoreRequest;
 use App\Http\Resources\GameStateResource;
@@ -9,9 +10,21 @@ use App\Models\Hand;
 
 class HoleCardStoreRequestHandler
 {
+    public function __construct(private HoleCardBuilder $builder)
+    {
+    }
+
     public function handle(HoleCardStoreRequest $request): GameStateResource
     {
         $hand = Hand::find($request->input('hand_id'));
+
+        $this->builder->buildHoleCard(
+            $request->input('player_id'),
+            $request->input('card_id'),
+            $request->input('face_up'),
+            $hand->id
+        );
+
         $gameState = GameState::fromGame($hand->game);
     
         return GameStateResource::make($gameState);
